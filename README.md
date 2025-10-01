d21c744ebc71fd1c5903aec44518fe3ad9c300b6
francisreyy <frey@fi.uba.ar>
2025-10-01 00:03:47 -0300

    feat: imágenes nuevas, se corrigen urls con url_for, información del evento para completar

--- README.md
+++ README.md
@@ -6,14 +6,21 @@ Repositorio para el EJ2 de IDS [TP: página web](https://github.com/francisreyy/
 - Francis Rey 102785
 - José Adrián
 
-## Dependencias
+## Dependencias para ejecutar la página web
+- completar
+
+## Dependencias para editar la página web
 - completar
 
 ## Crear entorno
+- Ejecutar el siguiente script en la terminal
 
 ```bash
 bash ./EjPractico2.sh
 ```
+
+## Preparación de archivos
+- Descargar el repo de github de la rama deploy y pegar el contenido de la carpeta TP2-IDS en la misma carpeta TP2-IDS creada por el script.
 
 ## Ejecutar la página
 
@@ -30,3 +37,6 @@ flask run --port=5050 --debug
 ```
 
 Entrar a la página: `localhost:5050`
+
+## Template
+- [Template: Soccer – Free Bootstrap HTML5 Sports Website Template](https://themewagon.com/themes/free-bootstrap-html5-sports-website-template-soccer/) de colorlib
--- TP2-IDS/app.py
+++ TP2-IDS/app.py
@@ -3,25 +3,26 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-info_evento = {
-    1: { "nombre": "Rally MTB 2025",
-    "organizador": "Club Social y Deportivo Unidos por el Deporte",
-    "descripcion": "Carrera de MTB rural en dos modalidades 30km y 80km …",
-    "fecha": "24 de Octubre de 2025",
-    "horario": "8am",
-    "lugar": "Tandil, Buenos Aires",
-    "tipo_carrera": "MTB rural",
-    "modalidad_costo": {1: {"nombre": "Corta" ,"valor": "100"},
-                        2: {"nombre": "Larga" ,"valor": "200"}},
-    "Auspiciantes": ["ausp1","auspN"]
-    }
-}
 
 
 @app.route("/")
 def index():
+    info_evento = {
+        1: { "nombre": "Rally MTB 2025",
+        "organizador": "Club Social y Deportivo Unidos por el Deporte",
+        "descripcion": "Carrera de MTB rural en dos modalidades 30km y 80km …",
+        "fecha": "24 de Octubre de 2025",
+        "horario": "8am",
+        "lugar": "Tandil, Buenos Aires",
+        "tipo_carrera": "MTB rural",
+        "modalidad_costo": {1: {"nombre": "Corta" ,"valor": "100"},
+                            2: {"nombre": "Larga" ,"valor": "200"}},
+        "Auspiciantes": ["ausp1","auspN"]
+        }
+    }
+
     title = "Club Social y Deportivo Unidos por el Deporte"
-    return render_template('index.html',page_title = title)
+    return render_template('index.html',page_title = title, info = info_evento)
 
 @app.route("/registration")
 def registration():
@@ -29,4 +30,4 @@ def registration():
     return render_template('registration.html',page_title = title)
 
 if __name__ == "__main__":
-    app.run("127.0.0.1", port="5000", debug=True)
+    app.run("localhost", port="5000", debug=True)
